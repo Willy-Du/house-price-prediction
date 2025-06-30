fdfae70f683c18064d6f3f17618aa571d2476724
Willy <willy.du@efrei.net>
2025-06-30 17:06:05 +0200

    Fix PYTHONPATH for GitHub Actions

--- backend/test/test_predict.py
+++ backend/test/test_predict.py
@@ -1,6 +1,10 @@
 import pytest
-from fastapi.testclient import TestClient
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import app
+from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
